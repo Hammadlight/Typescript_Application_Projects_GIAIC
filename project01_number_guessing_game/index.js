0da6edc19f9d63f7858648d49639330898a20675
Hammad Noor Khan <147694528+Hammadlight@users.noreply.github.com>
2024-08-01 07:10:27 -0700

    Add files via upload

---
+++ project01_number_guessing_game/index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log("\n\tWellcome to the Hammad's Game - CLI Number Guessing Game\n");
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Enter your guess number(Number Limit from 1 to 5):",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congratulation ! You guessed a correct number.");
+}
+else {
+    console.log("Sorry, you guess a wrong number");
+}
